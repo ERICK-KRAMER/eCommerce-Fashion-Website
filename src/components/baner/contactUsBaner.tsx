4a5217f37d325ad587624092ce47e4f60dd59f1a
ERICK-KRAMER <kramerfilho@hotmail.com>
2024-05-28 16:53:12 -0300

    style: adjust syze baner

--- src/components/baner/contactUsBaner.tsx
+++ src/components/baner/contactUsBaner.tsx
@@ -1,6 +1,6 @@
 const ContactUsBaner = () => {
   return (
-    <div className="bg-contactBaner bg-cover bg-center w-full h-[200px] flex p-6 items-end">
+    <div className="bg-contactBaner bg-cover bg-center w-full h-[250px] flex p-6 items-end">
       <h1 className="text-4xl text-white font-thin">CONTACT US</h1>
     </div>
   )
